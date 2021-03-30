8c8464f44887279001899f83a83f66084261025a
Manuel Martin <manuel.martin@gmail.com>
2021-03-30 23:38:14 +0200

    feat: add container styles
    closes #3

---
+++ test/container.css.test.js
@@ -0,0 +1,30 @@
+import { expect } from '@open-wc/testing';
+import container from '../src/container.css.js';
+
+describe('container', () => {
+  it('should generate the following css rules', async () => {
+    const rules = container({
+      first: true,
+      last: true,
+      odd: true,
+      even: true,
+      visited: true,
+      checked: true,
+      focusWithin: true,
+      hover: true,
+      focus: true,
+      focusVisible: true,
+      active: true,
+      disabled: true,
+      motionReduce: true,
+      motionSafe: true,
+      responsive: true,
+    });
+
+    const dataUrl = new URL('./container.css.json', import.meta.url);
+    const response = await fetch(dataUrl);
+    const json = await response.json();
+
+    expect(rules).to.be.eql(json);
+  });
+});
