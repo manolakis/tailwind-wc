a63a8e374c87bdf01c858faa735c45e785965a0e
Cesar Redondo Bueno <revuls@gmail.com>
2021-06-03 00:38:28 +0200

    feat: add flexbox rules

--- packages/core/src/RulesManager.js
+++ packages/core/src/RulesManager.js
@@ -1,10 +1,16 @@
+<<<<<<< HEAD
 import layoutResolver from './layout/resolver.js';
+=======
+import layoutResolver from './layoutResolver.js';
+import flexboxGridResolver from './flexboxGridResolver.js';
+>>>>>>> feat: add flexbox rules
 
 export class RulesManager {
   constructor() {
     this.__ruleResolvers = new Map();
 
     this.add(layoutResolver);
+    this.add(flexboxGridResolver);
   }
 
   add(resolver) {
